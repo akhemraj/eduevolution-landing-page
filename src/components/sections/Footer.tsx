9400ae2f64bf03ccf8bdc7466e300988c8d10e0f
rohanIndo9 <rohan@indorse.io>
2025-04-21 18:05:13 +0000

    refactor: footer UI change

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,99 +1,121 @@
 import React from "react";
-import { EnvelopeSimple } from "@phosphor-icons/react/dist/ssr";
+import {
+  EnvelopeSimple,
+  LinkedinLogo,
+  MapPin,
+} from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "../ui/button";
 
 function Footer() {
   return (
-    <div>
-      <footer className=" bg-blue-50 mt-0 ">
-        <div className="mx-auto w-full max-w-screen-xl px-[40px] py-[50px] md:px-0">
-          <div className="md:flex md:justify-between">
-            <div className="mb-28 md:mb-0">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap ark:text-blue-600">
-                <span className="self-center text-xl sm:text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  <div className="flex gap-2 md:gap-4 items-center">
-                    <Image
-                      src="/images/EduSage32x32.svg"
-                      alt="Brand-icon"
-                      width={46}
-                      height={46}
-                      className="w-10 h-10 md:w-[46px] md:h-[46px]"
-                      priority
-                    />
-                    <div className="flex flex-col">
-                      <h2 className="text-[22px] md:text-2xl text-blue-600 font-bold">
-                        EduSage<span className="text-gray-800">AI</span>
-                      </h2>
-                      <p className="text-xs md:text-sm text-gray-700 font-normal">
-                        Revolutionizing Assignment Grading with AI
-                      </p>
-                    </div>
-                  </div>
-                </span>
-              </span>
-            </div>
-            <div className="grid grid-cols-1">
-              <div className="flex justify-center md:justify-start">
-                <Link href="mailto:contact@edusageai.com">
-                  <Button className="bg-transparent text-blue-600 border-2 border-blue-600 rounded-md gap-[10px] px-[30px] md:px-[18px] py-[24px] hover:bg-transparent">
-                    <EnvelopeSimple size={20} color="#1C64F2" />
-                    contact@edusageai.com
-                  </Button>
-                </Link>
+    <footer className="bg-blue-50">
+      <div className="w-full px-5 lg:mx-auto lg:px-0 pt-12 max-w-screen-xl">
+        <div className="flex flex-col space-y-10 lg:flex-row lg:space-y-0 justify-between">
+          <div>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <Image
+                  src="/images/EduSage32x32.svg"
+                  alt="Brand-icon"
+                  width={46}
+                  height={46}
+                  className="w-10 h-10"
+                  priority
+                />
+                <h2 className="text-[22px] md:text-2xl text-blue-600 font-bold">
+                  EduSage<span className="text-gray-800">AI</span>
+                </h2>
               </div>
+              <p className="text-sm font-medium text-gray-700 max-w-[310px]">
+                Revolutionize grading with AI-powered speed, accuracy, and
+                smarter insights
+              </p>
+            </div>
+            <div className="mt-8 flex items-center space-x-3">
+              <Link
+                href="https://www.linkedin.com/company/edusage-ai/"
+                target="_blank"
+                className="flex justify-center items-center bg-blue-200 hover:bg-blue-100 transition-colors duration-300 ease-in-out w-10 h-10 rounded-full"
+              >
+                <LinkedinLogo size={22} color="#1C64F2" weight="bold" />
+              </Link>
+              <Link
+                href="mailto:contact@edusageai.com"
+                target="_blank"
+                className="flex justify-center items-center bg-blue-200 hover:bg-blue-100 transition-colors duration-300 ease-in-out w-10 h-10 rounded-full"
+              >
+                <EnvelopeSimple size={22} color="#1C64F2" weight="bold" />
+              </Link>
             </div>
           </div>
-          <hr className="my-6 border-white sm:mx-auto dark:border-white lg:my-8" />
-          <div className="sm:flex sm:items-center sm:justify-center">
-            <div className="flex text-[10px] md:text-xs sm:text-sm mb-3 md:mb-0 justify-center items-center space-x-5 text-gray-700 dark:text-gray-800 mr-3">
+          <div className="flex flex-wrap gap-14 lg:gap-24 lg:flex-nowrap">
+            <div className="flex flex-col gap-2">
+              <h5 className="text-gray-500 text-sm uppercase font-semibold mb-2 lg:mb-3">
+                Company
+              </h5>
               <Link
-                href={"/PrivacyPolicy"}
-                className="text-xs cursor-pointer hover:text-blue-600"
+                href="/about-us"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
+              >
+                About us
+              </Link>
+              <Link
+                href="#Features"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
+              >
+                Features
+              </Link>
+              <Link
+                href="/pricing"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
+              >
+                Pricing
+              </Link>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h5 className="text-gray-500 text-sm uppercase font-semibold mb-2 lg:mb-3">
+                Help
+              </h5>
+              <Link
+                href="/PrivacyPolicy"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
               >
                 Privacy Policy
               </Link>
               <Link
-                href={"/about-us"}
-                className="text-xs cursor-pointer hover:text-blue-600"
+                href="/TermsAndConditionsSection"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
               >
-                About Us
+                Terms & Conditions
               </Link>
               <Link
-                href={"/contact-us"}
-                className="text-xs cursor-pointer hover:text-blue-600"
+                href="/cancellation-and-refund"
+                className="text-sm font-semibold text-gray-800 hover:text-blue-600"
               >
-                Contact Us
-              </Link>
-              <Link
-                href={"/pricing"}
-                className="text-xs cursor-pointer hover:text-blue-600"
-              >
-                Pricing
-              </Link>
-              <Link
-                href={"/cancellation-and-refund"}
-                className="text-xs cursor-pointer hover:text-blue-600"
-              >
-                Cancellation/ Refund Policy
-              </Link>
-              <Link
-                href={"/TermsAndConditionsSection"}
-                className="text-xs cursor-pointer hover:text-blue-600 "
-              >
-                Terms And Conditions
+                Cancellation / Refund Policy
               </Link>
             </div>
-
-            <p className="text-[10px] md:text-xs sm:text-sm text-gray-700 text-center dark:text-gray-400">
-              © Copyright {new Date().getFullYear()}, All rights reserved by
-              EduSageAI
-            </p>
+            <div className="flex flex-col gap-2">
+              <h5 className="text-gray-500 text-sm uppercase font-semibold mb-2 lg:mb-3">
+                Contact us
+              </h5>
+              <div className="flex gap-2">
+                <MapPin size={20} color="#1F2A37" />
+                <p className="text-sm font-semibold text-gray-700 max-w-[250px]">
+                  Office 28, AB Ajinkya Apt, Lokmanyanagar, Pune - 411030,
+                  Maharashtra, India
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </footer>
-    </div>
+        <div className="w-full flex justify-center mt-10 pt-5 pb-8 border-t-2 border-gray-50 text-xs text-gray-500 ">
+          © Copyright {new Date().getFullYear()}, All rights reserved by
+          <span className="font-medium pl-1">EduSageAI</span>
+        </div>
+      </div>
+    </footer>
   );
 }
 
